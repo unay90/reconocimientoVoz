4830f07a5a3fe8656e18f1a472271885239c98bc
Unai Lopez <unay.lopez@gmail.com>
2014-06-23 20:26:17 +0200

    create class speaker

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import voice.RecognizeVoice;
+import voice.Speaker;
+
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
@@ -10,25 +12,10 @@ public class Main {
 		//Thread hiloEscucha = new Thread(new RecognizeVoice());
         //hiloEscucha.start();
 	//}
-
-	private static final String VOICENAME_kevin = "kevin16";
-	 private String text; // string to speech
-	 
-	 public Main(String text) {
-	  this.text = text;
-	 }
-	 
-	 public void speak() {
-	  Voice voice;
-	  VoiceManager voiceManager = VoiceManager.getInstance();
-	  voice = voiceManager.getVoice(VOICENAME_kevin);
-	  voice.allocate();
-	  voice.speak(text);
-	 }
 	 
 	 public static void main(String[] args) {
 	  String text = "Baron in 10 seconds. Dragon in 20 seconds";
-	  Main freeTTS = new Main(text);
+	  Speaker freeTTS = new Speaker(text);
 	  freeTTS.speak();
 	 }
 }
