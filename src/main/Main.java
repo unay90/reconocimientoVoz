22184739e794805bc031c82298f3f0e64281bcce
Unai Lopez <unay.lopez@gmail.com>
2014-07-03 11:13:24 +0200

    change file names

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
             if (args.length > 0) {
                 url = new File(args[0]).toURI().toURL();
             } else {
-                url = Main.class.getResource("helloworld.config.xml");
+                url = Main.class.getResource("reconocimientoVoz.config.xml");
             }
 
             System.out.println("Loading...");
@@ -28,48 +28,44 @@ public class Main {
 	    Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 	    Microphone microphone = (Microphone) cm.lookup("microphone");
 
+        /* allocate the resource necessary for the recognizer */
+        recognizer.allocate();
 
-            /* allocate the resource necessary for the recognizer */
-            recognizer.allocate();
-
-            /* the microphone will keep recording until the program exits */
-	    if (microphone.startRecording()) {
-
-		System.out.println
-		    ("Say: (Good morning | Hello) " +
-                     "( Bhiksha | Evandro | Paul | Philip | Rita | Will )");
-
-		while (true) {
-		    System.out.println
-			("Start speaking. Press Ctrl-C to quit.\n");
-
-                    /*
-                     * This method will return when the end of speech
-                     * is reached. Note that the endpointer will determine
-                     * the end of speech.
-                     */ 
-		    Result result = recognizer.recognize();
-		    
-		    if (result != null) {
-			String resultText = result.getBestFinalResultNoFiller();
-			System.out.println("You said: " + resultText + "\n");
+	        /* the microphone will keep recording until the program exits */
+		    if (microphone.startRecording()) {
+		    	System.out.println("Say: (Computer) (One | Two) to test");
+		    	System.out.println("Say: (Computer) (Dragon | Baron)");
+		    	while (true) {
+		    		System.out.println
+		    		("Start speaking. Press Ctrl-C to quit.\n");
+	
+	                /*
+	                 * This method will return when the end of speech
+	                 * is reached. Note that the endpointer will determine
+	                 * the end of speech.
+	                 */ 
+		    		Result result = recognizer.recognize();
+	
+				    if (result != null) {
+					String resultText = result.getBestFinalResultNoFiller();
+					System.out.println("You said: " + resultText + "\n");
+				    } else {
+					System.out.println("I can't hear what you said.\n");
+				    }
+		    	}
 		    } else {
-			System.out.println("I can't hear what you said.\n");
+				System.out.println("Cannot start microphone.");
+				recognizer.deallocate();
+				System.exit(1);
 		    }
-		}
-	    } else {
-		System.out.println("Cannot start microphone.");
-		recognizer.deallocate();
-		System.exit(1);
-	    }
         } catch (IOException e) {
-            System.err.println("Problem when loading HelloWorld: " + e);
+            System.err.println("Problem when loading ReconocimientoVoz: " + e);
             e.printStackTrace();
         } catch (PropertyException e) {
-            System.err.println("Problem configuring HelloWorld: " + e);
+            System.err.println("Problem configuring ReconocimientoVoz: " + e);
             e.printStackTrace();
         } catch (InstantiationException e) {
-            System.err.println("Problem creating HelloWorld: " + e);
+            System.err.println("Problem creating ReconocimientoVoz: " + e);
             e.printStackTrace();
         }
     }
